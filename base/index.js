1f4fb1e0cecc0ef25eab2ba773133dbb5fc43827
Maksim Romanov <maksim.romanov@jetrockets.pro>
2021-11-27 14:47:08 +0300

    Initial commit

---
+++ base/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  plugins: ["import", "modules-newline"],
+  rules: {
+    semi: [2, "always"],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    "object-curly-spacing": ["error", "always"],
+    "object-curly-newline": ["error", { multiline: true, minProperties: 4 }],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", ["parent", "sibling"]],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        moduleDirectory: ["node_modules", ""],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
