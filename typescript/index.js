d4ede83f46443ad6cf8c192ac3c49f6ccf3b5524
Maksim Romanov <maksim.romanov@jetrockets.pro>
2021-12-04 22:48:48 +0300

    fixed ts eslint | added tsconfig

--- typescript/index.js
+++ typescript/index.js
@@ -1,14 +1,22 @@
 module.exports = {
+  "parser": require.resolve('@typescript-eslint/parser'),
+  "parserOptions": {
+		"warnOnUnsupportedTypeScriptVersion": false,
+		"sourceType": 'module',
+		"project": 'tsconfig.json'
+	},
+
   "extends": ['plugin:@typescript-eslint/recommended'],
-  "parser": '@typescript-eslint/parser',
   "plugins": ['@typescript-eslint'],
-  '@typescript-eslint/explicit-module-boundary-types': 0,
-  '@typescript-eslint/no-explicit-any': 0,
-  '@typescript-eslint/ban-types': [
-    'error',
-    {
-      "extendDefaults": true,
-      "types": { '{}': false, "object": false },
-    },
-  ],
+  "rules": {
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        "extendDefaults": true,
+        "types": { '{}': false, "object": false },
+      },
+    ],
+  }
 };
