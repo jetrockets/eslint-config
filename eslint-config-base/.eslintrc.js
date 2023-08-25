22f355f4e1180f362ad6a77cb01204c29a76647f
Maksim Romanov <maksim.romanov@jetrockets.com>
2023-08-25 15:01:56 +0400

    Added import order rules

--- eslint-config-base/.eslintrc.js
+++ eslint-config-base/.eslintrc.js
@@ -3,7 +3,16 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: 'standard',
+  extends: [
+    'standard',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
+  plugins: [
+    'import'
+  ],
   overrides: [
     {
       env: {
@@ -22,5 +31,22 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    'prettier/prettier': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling']
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
   }
 }
