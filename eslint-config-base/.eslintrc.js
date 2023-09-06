b05d19733e24a58d74c536f5cd5739bbc2d969d0
Ivan Merkushkin <ivan.merkushkin@jetrockets.com>
2023-09-06 17:52:58 +0400

    Fixes for base config

--- eslint-config-base/.eslintrc.js
+++ eslint-config-base/.eslintrc.js
@@ -1,58 +1,45 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'standard',
     'plugin:import/recommended',
     'plugin:import/errors',
-    'plugin:import/warnings'
+    'plugin:import/warnings',
+    'plugin:prettier/recommended'
   ],
-  plugins: [
-    'import',
-    '@html-eslint',
-    'json-format',
-    '@cspell'
-  ],
+  plugins: ['import', '@html-eslint', 'json-format', '@cspell', 'prettier'],
+
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
+        sourceType: 'script'
+      }
     },
     {
       files: ['*.html'],
       parser: '@html-eslint/parser',
-      extends: ['plugin:@html-eslint/recommended'],
+      extends: ['plugin:@html-eslint/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
+
   rules: {
-    indent: ['error', 2],
-    'prettier/prettier': 'off',
-
-    'comma-dangle': ['error', {
-      objects: 'always',
-      arrays: 'never',
-      imports: 'never',
-      exports: 'never',
-      functions: 'never',
-    }],
-
+    indent: ['error', 2, { SwitchCase: 1 }], // switch-case fix
+    camelcase: 1,
     '@cspell/spellchecker': [
       'warn',
       {
-        checkComments: false,
+        checkComments: false
       }
     ],
 
@@ -60,17 +47,9 @@ module.exports = {
       'error',
       {
         'newlines-between': 'always',
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          ['parent', 'sibling']
-        ],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+        alphabetize: { order: 'asc', caseInsensitive: true }
       }
-    ],
-  },
+    ]
+  }
 }
